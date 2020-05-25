a43abc3b45969b63e14d421b2f88ac4d17a456b2
toxxxa <toxxxa@gmail.com>
2020-05-25 16:45:50 +0300

    new[account]: Возможность указать пароль при создании абонента через дилерское АПИ

--- raml2html.sh
+++ raml2html.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( dirname "$0" )"
 echo "Processed $DIR/api.raml > $DIR/index.html"
-raml2html "$DIR/api.raml" > "$DIR/index.html"
+raml2html \
+-o "$DIR/index.html" \
+-i "$DIR/api.raml"
+#--theme 'raml2html-slate-theme'
